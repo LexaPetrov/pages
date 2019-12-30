88593568391caaf87aa1a697daba7fcb9f91fc07
Алексей Петров <al1111997@yandex.ru>
2019-12-31 00:20:26 +0500

    'a'

--- jsplayer/data.js
+++ jsplayer/data.js
@@ -1,9 +1,9 @@
 let data = [
     {
         id: 0,
-        title: 'TrackName',
-        link: 'audio/songname.mp3',
-        isToRender: 0
+        title: 'ROXETTE - Knockin` on Every Door',
+        link: 'audio/ROXETTE - Knockin` on Every Door.mp3',
+        isToRender: 1
     },
     {
         id: 1,
@@ -19,8 +19,20 @@ let data = [
     },
     {
         id: 3,
-        title: 'Tadssddsdshe White Stripes - Seven Nation Army',
-        link: 'audio/The White Stripes - Seven Nation Army.mp3',
-        isToRender: 0
+        title: 'Би-2 - Варвара',
+        link: 'audio/Би-2 - Варвара.mp3',
+        isToRender: 1
+    },
+    {
+        id: 4,
+        title: 'Би-2 - Полковник',
+        link: 'audio/Би-2 - Полковник.mp3',
+        isToRender: 1
+    },
+    {
+        id: 5,
+        title: 'Сплин - Выхода нет',
+        link: 'audio/Сплин - Выхода нет.mp3',
+        isToRender: 1
     }
 ]
--- jsplayer/script.js
+++ jsplayer/script.js
@@ -1,17 +1,17 @@
-let songs = JSON.parse(JSON.stringify(data))
-console.log("Всего треков: ", songs.length)
+let songs = JSON.parse(JSON.stringify(data));
+console.log("Всего треков: ", songs.length);
 //render declaration
 const Render = (_id, divToRender) => {
-  let i = _id
+  let i = _id;
   if (songs[i].isToRender) {
-    let div = `<div class="player">`
+    let div = `<div class="player">`;
     div += `<div class="title"><p id="title${i}" >${songs[i].title}</p> <div class="controlsUpload">
     <a class='icon upload' title='Загрузить трек' href='javascript: void(0)' onclick="Upload()">
         <img src="img/upload.png" alt="upload_img">
     </a>
-    </div></div>`
-    div += `<audio id='audio${i}' src="${songs[i].link}" preload="auto"></audio>`
-    div += `<div class="controls">`
+    </div></div>`;
+    div += `<audio id='audio${i}' src="${songs[i].link}" preload="auto"></audio>`;
+    div += `<div class="controls">`;
     div += `<div class="controlsPlay">
                   <a id='play${i}' href="javascript:void(0)" class="icon play" onclick="Play(${songs[i].id})">
                       <img src="img/play.png" alt="play_img">
@@ -19,16 +19,16 @@ const Render = (_id, divToRender) => {
                   <a href="javascript:void(0)" class="icon pause" id='pause${i}' onclick="Play(${songs[i].id})">
                       <img src="img/pause.png" alt="pause_img">
                   </a>
-                  </div>`
-    div += `<span class="currentTime"><small id='currentTime${i}'>00:00</small></span>`
-    div += `<div class='progressbar'><progress id=progressbar${i} value='0' max='100'></progress></div>`
-    div += `<span class="allTime" ><small id="allTime${i}">00:00</small></span>`
+                  </div>`;
+    div += `<span class="currentTime"><small id='currentTime${i}'>00:00</small></span>`;
+    div += `<div class='progressbar'><progress id=progressbar${i} value='0' max='100'></progress></div>`;
+    div += `<span class="allTime" ><small id="allTime${i}">00:00</small></span>`;
     div += `<div class="controlsVolume">
-                  <a class='icon volume' onclick="Mute(${songs[i].id});">
-                      <img src="img/volume.png" alt="volume_img">
+                  <a class='icon volume'  onclick="Mute(${songs[i].id});">
+                      <img id='volumeImg${_id}' src="img/volume.png" alt="volume_img">
                   </a>
                   <input type='range' value='100' max='100' title='Громкость' id='volumeInput${i}' class='volumeInput'>
-                  </div>`
+                  </div>`;
     div += `<div class="controlsSpeed">
                   <select title='Скорость' id='controlsSpeedSelect${i}'>
                       <option value='50'>0.5X</option>
@@ -39,65 +39,108 @@ const Render = (_id, divToRender) => {
                       <option value='200'>2X</option>
                       <option value='200'>2.25X</option>
                   </select>
-                  </div>`
+                  </div>`;
     div += `<div class="controlsDownload">
                   <a class='icon download' title='Скачать трек' href='${songs[i].link}' download>
                       <img src="img/download.png" alt="download_img">
                   </a>
                   </div>
-                  `
-    div += `</div>`
-    div += `</div>`
+                  `;
+    div += `</div>`;
+    div += `</div>`;
     document.getElementById(divToRender.toString()).innerHTML += div;
-    window['audio' + i] = document.getElementById(`audio${i}`)
-    window['audio' + i].src = `${songs[i].link}`
+    window["audio" + i] = document.getElementById(`audio${i}`);
+    window["audio" + i].src = `${songs[i].link}`;
   }
-}
+};
+const ChangeSpeed = _id => {
+  window["audio" + _id].playbackRate =
+    document.getElementById("controlsSpeedSelect" + _id).value / 100;
+};
 //Normalize Time
-const NormalDuration = (time) => {
-  time = Math.floor(time)
-  let min = Math.floor(time / 60)
-  let sec = Math.floor(time - min * 60)
-  let minValue = min
-  let secValue = sec
-  min < 10 ? (minValue = '0' + min) : null
-  sec < 10 ? (secValue = '0' + sec) : null
-  return minValue.toString() + ':' + secValue.toString()
-}
-//PropgressBar
-const Progress = (_id) => {
-  let progressBar = document.getElementById('progressbar'+_id)
-  let currentTime = document.getElementById('currentTime'+_id)
-  progress = (Math.floor(window['audio' + _id].currentTime) / (Math.floor(window['audio' + _id].duration) / 100))
-  progressBar.value = progress
-  currentTime.innerHTML = NormalDuration(window['audio' + _id].currentTime)
-}
+const NormalDuration = time => {
+  time = Math.floor(time);
+  let min = Math.floor(time / 60);
+  let sec = Math.floor(time - min * 60);
+  let minValue = min;
+  let secValue = sec;
+  min < 10 ? (minValue = "0" + min) : null;
+  sec < 10 ? (secValue = "0" + sec) : null;
+  return minValue.toString() + ":" + secValue.toString();
+};
+//ProgressBar
+const Progress = _id => {
+  let progressBar = document.getElementById("progressbar" + _id);
+  let currentTime = document.getElementById("currentTime" + _id);
+  progress =
+    Math.floor(window["audio" + _id].currentTime) /
+    (Math.floor(window["audio" + _id].duration) / 100);
+  progressBar.value = progress;
+  currentTime.innerHTML = NormalDuration(window["audio" + _id].currentTime);
+};
 
-const Mute = () => { }
-const Upload = () => {console.log('upload called')}
-const Play = (_id) => {
-  _id = songs[_id].id
-  //let audio = document.getElementById(`audio${_id}`)
-  //audio.src = `${songs[_id].link}` 
-  let playButton = document.getElementById(`play${_id}`)
-  let pauseButton = document.getElementById(`pause${_id}`)
-  let duration = document.getElementById(`allTime${_id}`)
-  if (playButton.style.display === "block") {
-    playButton.style.display = "none"
-    pauseButton.style.display = "block"
-    window['audio' + _id].play()
+const Mute = _id => {
+  if (window["audio" + _id].volume === 0) {
+    window["audio" + _id].volume =
+      document.getElementById(`volumeInput${_id}`).value / 100;
+    document.getElementById(`volumeImg${_id}`).src = "./img/volume.png";
   } else {
-    window['audio' + _id].pause()
-    pauseButton.style.display = "none"
-    playButton.style.display = "block"
+    window["audio" + _id].volume = 0;
+    document.getElementById(`volumeImg${_id}`).src = "./img/muted.png";
   }
-  duration.innerHTML == '00:00' ? duration.innerHTML = NormalDuration(window['audio' + _id].duration) : null
-  window['audio' + _id].addEventListener('timeupdate', () => Progress(_id))
-}
+};
+const ChangeVolume = _id => {
+  window["audio" + _id].volume =
+    document.getElementById(`volumeInput${_id}`).value / 100;
+  document.getElementById(`volumeImg${_id}`).src = "./img/volume.png";
+};
+const Rewind = (_id, e) => {
+  let progressBar = document.getElementById("progressbar" + _id);
+  let mouseX = Math.floor(e.pageX - progressBar.offsetLeft);
+  let progress = mouseX / (progressBar.offsetWidth / 100);
+  window["audio" + _id].currentTime =
+    window["audio" + _id].duration * (progress / 100);
+};
+const Upload = () => {
+  console.log("upload called");
+};
+const Play = _id => {
+  _id = songs[_id].id;
+  let playButton = document.getElementById(`play${_id}`);
+  let pauseButton = document.getElementById(`pause${_id}`);
+  let duration = document.getElementById(`allTime${_id}`);
+  let speedSelector = document.getElementById(`controlsSpeedSelect${_id}`);
+  let muteButton = document.getElementById(`volumeInput${_id}`);
+  let volumeSelector = document.getElementById(`volumeInput${_id}`);
+  let progressBar = document.getElementById("progressbar" + _id);
+  if (playButton.style.display === "block") {
+    playButton.style.display = "none";
+    pauseButton.style.display = "block";
+    window["audio" + _id].play();
+  } else {
+    window["audio" + _id].pause();
+    pauseButton.style.display = "none";
+    playButton.style.display = "block";
+  }
+  duration.innerHTML == "00:00"
+    ? (duration.innerHTML = NormalDuration(window["audio" + _id].duration))
+    : null;
+  window["audio" + _id].addEventListener("timeupdate", () => Progress(_id));
+  speedSelector.addEventListener("change", () => ChangeSpeed(_id));
+  muteButton.addEventListener("change", () => {
+    Mute(_id);
+  });
+  volumeSelector.addEventListener("change", () => ChangeVolume(_id));
+  progressBar.addEventListener("click", e => Rewind(_id, e));
+};
 //render call
 for (let i = 0; i < songs.length; i++) {
-  Render(i, "container")
+  Render(i, "container");
 }
+Render(3, "renderHere");
 
-Render(2, "renderHere")
-
+document.addEventListener("DOMContentLoaded", function(event) {
+  for (let i = 0; i < songs.length; i++) {
+    document.getElementById(`play${i}`).style.display = "block";
+  }
+});
