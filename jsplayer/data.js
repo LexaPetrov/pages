1366d218484b0318108774691c425a31256c8c43
Алексей Петров <al1111997@yandex.ru>
2019-12-30 14:00:59 +0500

    'jsplayer'

---
+++ jsplayer/data.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+        id: 0,
+        title: 'TrackName',
+        link: 'audio/songname.mp3',
+        isToRender: 0
+    },
+    {
+        id: 1,
+        title: 'Radiohead - Creep',
+        link: 'audio/Radiohead - Creep.mp3',
+        isToRender: 1
+    },
+    {
+        id: 2,
+        title: 'The White Stripes - Seven Nation Army',
+        link: 'audio/The White Stripes - Seven Nation Army.mp3',
+        isToRender: 1
+    },
+    {
+        id: 3,
+        title: 'Tadssddsdshe White Stripes - Seven Nation Army',
+        link: 'audio/The White Stripes - Seven Nation Army.mp3',
+        isToRender: 1
+    }
+]
