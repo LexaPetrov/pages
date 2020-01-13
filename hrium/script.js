cffdaf1a156158022faf10c1d23f8ac8239d9916
Алексей Петров <al1111997@yandex.ru>
2020-01-13 21:32:36 +0500

    'a'

--- hrium/script.js
+++ hrium/script.js
@@ -49,3 +49,14 @@ document.addEventListener("DOMContentLoaded", () => {
 function toBuy() {
   document.getElementById('pdfcost').scrollIntoView({behavior: "smooth"});
 }
+
+function openbox(id,tt) {
+	var div = document.getElementById(id);
+	var tt_div = document.getElementById(tt);
+	if(div.style.display == 'block') {
+		div.style.display = 'none';
+	}
+	else {
+		div.style.display = 'block';
+	}
+  }
